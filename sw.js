4c9c9cce2f8cbbcecb477460df56777e9b5512f9
Teende Exorter Yoasngar <exhorterteende@gmail.com>
2026-01-01 05:11:37 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -19,25 +19,4 @@ self.addEventListener('fetch', event => {
     caches.match(event.request)
       .then(response => response || fetch(event.request))
   );
-});const CACHE_NAME = 'vaxitrack-v2.0-cache';
-const urlsToCache = [
-  './',
-  './index.html',
-  './style.css',
-  './app.js',
-  './manifest.json'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
 });
